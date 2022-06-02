ab912e20d329c653872d519ce91829c09a72ae7c
yanrongxing <1124182718@qq.com>
2022-06-02 15:45:47 +0800

    123

--- src/logic/address.ts
+++ src/logic/address.ts
@@ -9,6 +9,6 @@ const ERC1155_LOCAL = '0x610178da211fef7d417bc0e6fed39f05609ad788'
 const ERC1155_MATIC = '0x31f325a9d4e7e8e8a2e8b3f4a2ed20fc4e907133'
 const ERC1155_ADDREDSS = [ERC1155_LOCAL,ERC1155_MATIC]
 export const isERC1155 = (addr: string) => {
-  return ERC1155_ADDREDSS.indexOf(addr.toLocaleLowerCase()) > 0
+  return addr && ERC1155_ADDREDSS.indexOf(addr.toLocaleLowerCase()) > 0
 }
 
