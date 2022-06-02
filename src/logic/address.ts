ab6cc74b9f12961b2adb9233fc0ab516f74dcc06
yanrongxing <1124182718@qq.com>
2022-06-02 15:36:42 +0800

    fix: 大小写

--- src/logic/address.ts
+++ src/logic/address.ts
@@ -6,9 +6,9 @@ export const isAddressZero = (addr: string) => {
   return /^0x(0)+$/.test(addr)
 }
 const ERC1155_LOCAL = '0x610178da211fef7d417bc0e6fed39f05609ad788'
-const ERC1155_MATIC = '0x31f325A9D4E7e8E8A2E8b3f4a2eD20Fc4E907133'
+const ERC1155_MATIC = '0x31f325a9d4e7e8e8a2e8b3f4a2ed20fc4e907133'
 const ERC1155_ADDREDSS = [ERC1155_LOCAL,ERC1155_MATIC]
 export const isERC1155 = (addr: string) => {
-  return ERC1155_ADDREDSS.indexOf(addr) > 0
+  return ERC1155_ADDREDSS.indexOf(addr.toLocaleLowerCase()) > 0
 }
 
